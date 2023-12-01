fb818482871ee379734962236d427e1249dc8c75
ZidanAlfarizaPutraPratama <zidanalfariza@gmail.com>
2023-12-01 14:07:18 +0700

    restructure with hapi plugin

---
+++ src/api/notes/routes.js
@@ -0,0 +1,29 @@
+const routes = (notesHandler) => [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: notesHandler.postNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: notesHandler.getNoteByIdHandler, 
+    },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: notesHandler.getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: notesHandler.putNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: notesHandler.deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
